4691497ad06aff514cca9a031ebf68a80b5d65f7
Mustafa <it@hajtak.net>
2021-10-10 13:00:13 +0200

    commit 2

---
+++ postcss.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  plugins: [require("tailwindcss"), require("autoprefixer")],
+};
+const purgecss = require("@fullhuman/postcss-purgecss");
+const cssnano = require("cssnano");
+module.exports = {
+  plugins: [
+    require("tailwindcss"),
+    require("autoprefixer"),
+    cssnano({
+      preset: "default",
+    }),
+    purgecss({
+      content: ["./public/*.html"],
+      defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+    }),
+  ],
+};
